82daec1b6586e516281bfd9660fbd23eb3d47edd
Daniele Strigaro <daniele.strigaro@supsi.ch>
2024-07-11 09:48:07 +0200

    feat: added params for pool conn pg, added workerks and other params for uvicorn

--- fastapi/app/models/database.py
+++ fastapi/app/models/database.py
@@ -6,7 +6,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_async_engine(
-    os.getenv("DATABASE_URL"), pool_size=20, max_overflow=0
+    os.getenv("DATABASE_URL"),
+    pool_size=os.getenv("PG_POOL_SIZE"),  # Dimensione del pool di connessioni
+    max_overflow=os.getenv(
+        "PG_MAX_OVERFLOW"
+    ),  # Numero di connessioni extra che possono essere create se il pool è pieno
+    pool_timeout=os.getenv(
+        "PG_POOL_TIMEOUT"
+    ),  # Tempo di attesa prima di lanciare un timeout
+    pool_recycle=3600,  # Riciclo delle connessioni dopo un'ora
+    pool_pre_ping=True,  # Controllo delle connessioni prima di riutilizzarle
 )
 
 # Create the asynchronous session
