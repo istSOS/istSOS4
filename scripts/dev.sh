fdd5b27ef588d411d43642257a1c8c4e4ba1546d
Daniele Strigaro <daniele.strigaro@supsi.ch>
2024-11-11 12:38:42 +0100

    removed unused folders

--- scripts/dev.sh
+++
@@ -1,7 +0,0 @@
-# Check if device is a macbook with m1 chip
-
-if [[ $(uname -m) == "arm64" ]]; then
-    export DOCKER_DEFAULT_PLATFORM=linux/amd64
-fi
-
-docker compose -f dev_docker-compose.yml up --build
