b82241455858234a32e7e305f0292c270f2bd66e
Claudio <claprime2498@gmail.com>
2025-05-06 17:58:25 +0200

    fix: ensure postgis extension is created in the database schema

--- database/istsos_schema.sql
+++ database/istsos_schema.sql
@@ -12,6 +12,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS btree_gist;
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
